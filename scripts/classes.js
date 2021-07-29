0fc30ca7b466fa7020f59e52dea3522da0f6becd
prajwalpokharel <84442431+prajwalpokharel@users.noreply.github.com>
2021-07-29 20:14:04 +0545

    comment some stuff
    Signed-off-by: prajwalpokharel <84442431+prajwalpokharel@users.noreply.github.com>

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,8 +1,18 @@
+/*
+    Copyright (c) 2021 soulr344 @ github
+*/
+
+/**
+ * Adds a click listener to a pre-existing element `selector` that
+ * scrolls the page to the top when clicked.
+ * @param {string} selector The selector to add the clickable feature.
+ * @return {void}
+*/
 class scrollToTop {
-    constructor(query){
-        const element = document.querySelector(query)
+    constructor(selector){
+        const element = document.querySelector(selector)
         if (!element){
-            console.log("Failed to initialize scrollToTop module! No elements with selector", query, "found!");
+            console.log("Failed to initialize scrollToTop module! No elements with selector", selector, "found!");
             return null;
         }
         window.addEventListener("scroll", () => {
@@ -25,8 +35,15 @@ class scrollToTop {
     }
 }
 
+/**
+ * Toggles a class of a burger div and a navigation panel to create an animated navbar.
+ * @param {string} burgerSelector The selector of burger div.
+ * @param {string} panelSelector The selector of navigation panel.
+ * @param {Function} extraStuff Callback function that runs on every click to the burger.
+ * @return {void}
+*/
 class AnimatedNavBar {
-    constructor(burgerSelector, panelSelector, extraStuff = function(){}){
+    constructor(burgerSelector, panelSelector, extraStuff = null){
         var burger = document.querySelector(burgerSelector),
             panel = document.querySelector(panelSelector);
 
@@ -37,7 +54,7 @@ class AnimatedNavBar {
         burger.addEventListener("click", ()=>{
             burger.classList.toggle("clicked");
             panel.classList.toggle("show");
-            extraStuff();
+            if (extraStuff != null) extraStuff();
         });
     }
 }
