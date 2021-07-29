64b7bbab7c6189431a2a81b992f4174db1d33bb9
prajwalpokharel <84442431+prajwalpokharel@users.noreply.github.com>
2021-07-29 17:24:27 +0545

    classify some js
    Signed-off-by: prajwalpokharel <84442431+prajwalpokharel@users.noreply.github.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,11 @@
-window.onload = () => {
-    const burger = document.querySelector(".burger-menu"),
-          navpane = document.querySelector("nav ul"),
-          topmain = document.querySelector(".top-main main"),
-          pcontainer = document.querySelector(".pointer-container");
+(() => {
+    new scrollToTop(".gototop");
+    new AnimatedNavBar(".burger-menu", ".top-main nav ul", () => {
+        const topmain = document.querySelector(".top-main main"),
+              pcontainer = document.querySelector(".pointer-container");
 
-    burger.addEventListener('click', () => {
-        burger.classList.toggle("clicked");
-        navpane.classList.toggle("show");
         document.body.classList.toggle("noscroll");
         topmain.classList.toggle("blur");
         pcontainer.classList.toggle("blur");
-    })
-
-    const learnmore = document.querySelector("#learn-more");
-    learnmore.addEventListener("click", () => {
-        document.querySelector("body > main").scrollIntoView({ 
-            behavior: 'smooth' 
-        });
     });
-}
-
-function scrollToTop(){
-    window.scroll({
-        top: 0, 
-        left: 0, 
-        behavior: 'smooth' 
-      });      
-}
+})();
